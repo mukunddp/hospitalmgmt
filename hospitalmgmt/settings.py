1b4d2f6962d8c2fee77d3f51292ac40bd6954e33
unknown <mukundparve98@gmail.com>
2022-03-25 00:13:39 +0530

    make it better

--- hospitalmgmt/settings.py
+++ hospitalmgmt/settings.py
@@ -81,28 +81,28 @@ WSGI_APPLICATION = 'hospitalmgmt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hospital',
-        'USER': 'postgres',
-        'PASSWORD': 'Mukund@123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': '<database_name>',
-#         'USER': '<user_name>',
-#         'PASSWORD': '<password>',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hospital',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Mukund@123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<database_name>',
+        'USER': '<user_name>',
+        'PASSWORD': '<password>',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 WHITENOISE_USE_FINDERS = True
